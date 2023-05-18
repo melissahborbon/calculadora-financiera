4e6d77d4fc46aad8d34889f9d467c97fb3f56671
melissahborbon <melissa.borbon@uabc.edu.mx>
2023-05-17 18:08:20 -0700

    V.1
    Signed-off-by: melissahborbon <melissa.borbon@uabc.edu.mx>

--- main.py
+++ main.py
@@ -20,13 +20,17 @@ class InteresSimple:
     def clasemadre():
         print(f"\n~Clase Madre Creada~")
 
+    def conversion_a_porcentaje(self):
+        self.tasa_interes = self.tasa_interes / 100
+        return self.tasa_interes
+
 
 class Interes(InteresSimple):
     def __init__(self, capital, tasa_interes, plazo, tipo_plazo, interes, monto):
         super().__init__(capital, tasa_interes, plazo, tipo_plazo, interes, monto)
 
     def calcular_interes(self):
-        self.tasa_interes = self.tasa_interes / 100
+        self.conversion_a_porcentaje()
         self.interes = self.capital * self.tasa_interes * self.plazo
         return self.interes
 
@@ -45,7 +49,7 @@ class Capital(InteresSimple):
         super().__init__(capital, tasa_interes, plazo, tipo_plazo, interes, monto)
 
     def calcular_capital(self):
-
+        self.conversion_a_porcentaje()
         self.capital = self.interes / (self.tasa_interes * self.plazo)
         return self.capital
 
@@ -64,6 +68,7 @@ class Plazo(InteresSimple):
         super().__init__(capital, tasa_interes, plazo, tipo_plazo, interes, monto)
 
     def calcular_plazo(self):
+        self.conversion_a_porcentaje()
         self.plazo = self.interes / (self.capital * self.tasa_interes)
         return self.plazo
 
@@ -74,13 +79,19 @@ class Plazo(InteresSimple):
 #         pass
 
 # Calcular Interes
+print(f"Calcular interes")
 interes_calculado = Interes(10000, 11, 5, "año", 0, 0)  # Creando una instancia
 print(interes_calculado.calcular_interes())  # Llamando al método
+print()
 
 # Calcular Monto
+print(f"Calcular monto")
 monto_calculado = Monto(10000, 0, 0, "año", 5500, 0)  # Creando una instancia
 print(monto_calculado.calcular_monto())  # Llamando al método
+print()
 
-#Calcular Capital
-capital_calculado = Capital( 0, 11, 5, "año", 0, 15500)
+# Calcular Capital
+print(f"Calcular capital")
+capital_calculado = Capital(0, 11, 5, "año", 5500, 0)
 print(capital_calculado.calcular_capital())
+print()
